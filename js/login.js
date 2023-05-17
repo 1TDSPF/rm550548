3b508c27b5ff88161f916f973a797738e6e81384
gutolive09 <gutolive01@gmail.com>
2023-05-17 11:50:52 -0300

    icone de olho

--- js/login.js
+++ js/login.js
@@ -21,10 +21,12 @@ console.log(listaDeUsuarios);
 
 addEventListener("click", (evt)=>{
     
+    const inputEmail = document.querySelector("#idEmail");
+    const inputPass = document.querySelector("#idPass");
+
     if(evt.target.id == "btnSubmit"){
         
-        const inputEmail = document.querySelector("#idEmail");
-        const inputPass = document.querySelector("#idPass");
+        
 
         try {
             
@@ -53,5 +55,14 @@ addEventListener("click", (evt)=>{
 
 
 
+    }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
+        if(inputPass.getAttribute("type") == "password"){
+            inputPass.setAttribute("type", "text");
+            evt.target.setAttribute("class", "fa fa-eye-slash")
+
+        }else{
+            inputPass.setAttribute("type", "password");
+            evt.target.setAttribute("class", "fa fa-eye")
+        }
     }
 });
